617141f4bfd947d33e72a3745a29d6d49ad65d3d
code@lookout.no <code@lookout.no>
2014-12-30 21:49:35 +0100

    Converted project from SVN to Git

---
+++ ButtonTracker.cpp
@@ -0,0 +1,91 @@
+/************************************************************************/
+/* ButtonTracker with Debounce Timer, v1.0.                             */
+/*                                                                      */
+/* This library is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* This library is distributed in the hope that it will be useful, but  */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     */
+/* General Public License for more details.                             */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with this library. If not, see <http://www.gnu.org/licenses/>. */
+/*                                                                      */
+/* Written by Thomas Mittet code@lookout.no January 2010.               */
+/************************************************************************/
+
+#include "WProgram.h"
+#include "ButtonTracker.h"
+
+ButtonTracker::ButtonTracker(uint16_t debounceTimeoutMs, uint16_t clickTimeoutMs, uint8_t buttonPin)
+{
+  this->debounceTimeoutMs = debounceTimeoutMs;
+  this->clickTimeoutMs = clickTimeoutMs;
+  this->buttonPin = buttonPin;
+  lastDebounce = millis();
+
+  pinMode(buttonPin, INPUT);
+  state = digitalRead(buttonPin);
+  previousState = state;
+}
+
+void ButtonTracker::update()
+{
+  //////////////////////////////
+  /// Debounce Timer
+  //////////////////////////////
+  bool stateChanged = 0;
+  bool pinState = digitalRead(buttonPin);
+  if (previousState != pinState)
+  {
+    lastDebounce = millis();
+  }
+  if (millis() - lastDebounce >= debounceTimeoutMs)
+  {
+    if(state != pinState)
+    {
+      state = pinState;
+      stateChanged = 1;
+    }
+  }
+  previousState = pinState;
+
+  //////////////////////////////
+  /// Button Tracker
+  //////////////////////////////
+  if(state)
+  {
+    // When button is down
+    if(stateChanged)
+    {
+      down = millis();
+      clicks++;
+      // Save last clicked
+      lastClicked = clicked;
+      clicked = millis();
+    }
+    // When button is being held down
+    else if(value < 0)
+    {
+      return;
+    }
+  }
+  value = 0;
+  if(clicks >= 1 && millis() - down > clickTimeoutMs)
+  {
+    if(state)
+    {
+      // Button is still down after click timeout, button is being held down
+      value = 0 - clicks;	
+    }
+    else
+    {
+      // Button has been released, return number of clicks
+      value = clicks;
+    }
+    clicks = 0;
+  }
+}
---
+++ ButtonTracker.h
@@ -0,0 +1,41 @@
+/************************************************************************/
+/* ButtonTracker with Debounce Timer, v1.0.                             */
+/*                                                                      */
+/* This library is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* This library is distributed in the hope that it will be useful, but  */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     */
+/* General Public License for more details.                             */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with this library. If not, see <http://www.gnu.org/licenses/>. */
+/*                                                                      */
+/* Written by Thomas Mittet code@lookout.no January 2010.               */
+/************************************************************************/
+
+#ifndef ButtonTracker_h
+#define ButtonTracker_h
+
+#include <inttypes.h>
+
+class ButtonTracker
+{
+
+public:
+  ButtonTracker(uint16_t debounceTimeoutMs, uint16_t clickTimeoutMs, uint8_t buttonPin);
+  void update();
+  int8_t value;
+  unsigned long clicked, lastClicked;
+  
+private:
+  uint16_t debounceTimeoutMs, clickTimeoutMs;
+  uint32_t lastDebounce, down;
+  uint8_t buttonPin, clicks;
+  bool state, previousState;
+};
+
+#endif
